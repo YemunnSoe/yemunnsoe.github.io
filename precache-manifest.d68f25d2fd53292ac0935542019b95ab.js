f0c00f472688eca666f694883d3ce580285a70af
D Ther <dtherhtun.cw@gmail.com>
2023-04-12 16:14:03 +0700

    Updates

---
+++ precache-manifest.d68f25d2fd53292ac0935542019b95ab.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5100a9045346a9930b79",
+    "url": "yemunnsoe.github.io/static/css/main.e331b353.chunk.css"
+  },
+  {
+    "revision": "5100a9045346a9930b79",
+    "url": "yemunnsoe.github.io/static/js/main.5100a904.chunk.js"
+  },
+  {
+    "revision": "aecb47be87bea160c595",
+    "url": "yemunnsoe.github.io/static/css/1.80ac61ad.chunk.css"
+  },
+  {
+    "revision": "aecb47be87bea160c595",
+    "url": "yemunnsoe.github.io/static/js/1.aecb47be.chunk.js"
+  },
+  {
+    "revision": "42c02612833787bfa0cf",
+    "url": "yemunnsoe.github.io/static/js/runtime~main.42c02612.js"
+  },
+  {
+    "revision": "23273bc8bc3197e29e433d7cf66185fc",
+    "url": "yemunnsoe.github.io/static/media/sklearn_genetic_opt.23273bc8.png"
+  },
+  {
+    "revision": "fed0850a36ae8cfbe9ceaa1ce970c5cc",
+    "url": "yemunnsoe.github.io/static/media/graph_embeddings.fed0850a.png"
+  },
+  {
+    "revision": "9c0f986795edbdc96c99bc8853580310",
+    "url": "yemunnsoe.github.io/static/media/kafkaml.9c0f9867.png"
+  },
+  {
+    "revision": "8ba027d94d0cd4d31608e2c6e43f9488",
+    "url": "yemunnsoe.github.io/static/media/scikit_pipes.8ba027d9.png"
+  },
+  {
+    "revision": "4b632d51a45152af9b70077a3d4955ca",
+    "url": "yemunnsoe.github.io/static/media/portfolio.4b632d51.png"
+  },
+  {
+    "revision": "f7d742fc14d3d495da2581043372d15a",
+    "url": "yemunnsoe.github.io/static/media/fastapi_sklearn.f7d742fc.png"
+  },
+  {
+    "revision": "265631d25684ce84fe37b4d112762cad",
+    "url": "yemunnsoe.github.io/index.html"
+  }
+];
